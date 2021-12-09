6f341bad94ce547aea607776d60c6f9b12960d7d
amohanan-scottlogic <amohanan@scottlogic.com>
2021-12-09 14:01:24 +0000

    All sorting changes

--- src/main/java/com/scottlogic/AuthorPostSorter.java
+++ src/main/java/com/scottlogic/AuthorPostSorter.java
@@ -1,43 +1,21 @@
 package com.scottlogic;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
-import static java.util.Comparator.comparing;
 
 public class AuthorPostSorter implements PostSorter {
 
 
     @Override
     public List<UserPost> sort(List<UserPost> inputList, SortOrder orderIn) {
-        List<UserPost> namesNull = new ArrayList<>();
-        List<UserPost> remainingPosts = new ArrayList<>();
-
-        for (int i = 0; i < inputList.size(); i++) {
-            if (inputList.get(i).getAuthor() == null) {
-                namesNull.add(inputList.get(i));
-            } else {
-                remainingPosts.add(inputList.get(i));
-            }
-        }
-
 
         switch (orderIn) {
 
-            case ASC -> Collections.sort(remainingPosts, Comparator.comparing(UserPost::getAuthor));
+            case ASC -> Collections.sort(inputList, Comparator.nullsLast(Comparator.comparing(UserPost::getAuthor, Comparator.nullsLast(Comparator.naturalOrder()))));
 
-            case DESC -> Collections.sort(remainingPosts, Collections.reverseOrder(comparing(UserPost::getAuthor)));
+            case DESC -> Collections.sort(inputList, Collections.reverseOrder(Comparator.nullsLast(Comparator.comparing(UserPost::getAuthor, Comparator.nullsFirst(Comparator.naturalOrder())))));
         }
-
-        if (namesNull != null) {
-            remainingPosts.addAll(namesNull);
-        }
-
-        return remainingPosts;
-
-
+        return inputList;
     }
-
 }
--- src/test/java/com/scottlogic/DatePostSorterTest.java
+++ src/test/java/com/scottlogic/DatePostSorterTest.java
@@ -10,19 +10,17 @@ import java.util.List;
 
 public class DatePostSorterTest {
 
-
     DatePostSorter sortByDate = new DatePostSorter();
-    private UserPost createUserPost (OffsetDateTime date ) {
+
+    private UserPost createUserPost(OffsetDateTime date) {
 
         return new UserPost("Joe BLoggs",
                 date,
                 "Hello World!", 2);
     }
 
-
-
     @Test
-    public void dateSorter_mixedValidNames_correctListReturnedAsc() {
+    public void sort_mixedValidDatesAsc_correctListReturned() {
         UserPost userPost1 = createUserPost(OffsetDateTime.of(20, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
         UserPost userPost2 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
         UserPost userPost3 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 1, ZoneOffset.UTC));
@@ -30,35 +28,69 @@ public class DatePostSorterTest {
         UserPost userPost5 = createUserPost(OffsetDateTime.of(2020, 12, 31, 7, 12, 3, 0, ZoneOffset.UTC));
 
         List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4, userPost5);
-        sortByDate.sort(userPosts, SortOrder.ASC);
+        List<UserPost> sortedBtMethod = sortByDate.sort(userPosts, SortOrder.ASC);
         List<UserPost> sorted = Arrays.asList(userPost1, userPost4, userPost2, userPost3, userPost5);
+        Assert.assertEquals(sortedBtMethod, sorted);
+    }
+
+    @Test
+    public void sort_oneObjectListAsc_oneObjectListReturned() {
+        UserPost userPost1 = createUserPost(OffsetDateTime.of(2012, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        List<UserPost> userPosts = Arrays.asList(userPost1);
+        sortByDate.sort(userPosts, SortOrder.ASC);
+        List<UserPost> sorted = Arrays.asList(userPost1);
         Assert.assertEquals(userPosts, sorted);
     }
-//
-//    @Test
-//    public void authorSorter_oneObjectList_oneObjectListReturnedAsc() {
-//        List<UserPost> userPosts = Arrays.asList(userPost1);
-//        dps.sorter(userPosts, SortOrder.ASC);
-//        List<UserPost> sorted = Arrays.asList(userPost1);
-//        Assert.assertEquals(userPosts, sorted);
-//    }
-//
-//
-//    @Test
-//    public void authorSorter_mixedValidNames_correctListReturnedDesc() {
-//        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4, userPost5);
-//        dps.sorter(userPosts, SortOrder.DESC);
-//        List<UserPost> sorted = Arrays.asList(userPost5, userPost3, userPost4, userPost2, userPost1);
-//        Assert.assertEquals(userPosts, sorted);
-//    }
-//
-//    @Test
-//    public void authorSorter_oneObjectList_oneObjectListReturnedDesc() {
-//        List<UserPost> userPosts = Arrays.asList(userPost1);
-//        dps.sorter(userPosts, SortOrder.DESC);
-//        List<UserPost> sorted = Arrays.asList(userPost1);
-//        Assert.assertEquals(userPosts, sorted);
-//    }
 
+    @Test
+    public void sort_nullDateAsc_nullLast() {
+        UserPost userPost1 = createUserPost(OffsetDateTime.of(20, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost2 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost3 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 1, ZoneOffset.UTC));
+        UserPost userPost4 = createUserPost(OffsetDateTime.of(2012, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost5 = createUserPost(OffsetDateTime.of(2020, 12, 31, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost6 = createUserPost(null);
 
+        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4, userPost5, userPost6);
+        List<UserPost> sortedBtMethod = sortByDate.sort(userPosts, SortOrder.ASC);
+        List<UserPost> sorted = Arrays.asList(userPost1, userPost4, userPost2, userPost3, userPost5, userPost6);
+        Assert.assertEquals(sortedBtMethod, sorted);
+    }
+    @Test
+    public void sort_mixedValidDatesDesc_correctListReturned() {
+        UserPost userPost1 = createUserPost(OffsetDateTime.of(20, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost2 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost3 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 1, ZoneOffset.UTC));
+        UserPost userPost4 = createUserPost(OffsetDateTime.of(2012, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost5 = createUserPost(OffsetDateTime.of(2020, 12, 31, 7, 12, 3, 0, ZoneOffset.UTC));
+
+        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4, userPost5);
+        List<UserPost> sortedBtMethod = sortByDate.sort(userPosts, SortOrder.DESC);
+        List<UserPost> sorted = Arrays.asList(userPost5, userPost3, userPost2, userPost4, userPost1);
+        Assert.assertEquals(sortedBtMethod, sorted);
+    }
+
+    @Test
+    public void sort_oneObjectListDesc_oneObjectListReturned() {
+        UserPost userPost1 = createUserPost(OffsetDateTime.of(2012, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        List<UserPost> userPosts = Arrays.asList(userPost1);
+        sortByDate.sort(userPosts, SortOrder.DESC);
+        List<UserPost> sorted = Arrays.asList(userPost1);
+        Assert.assertEquals(userPosts, sorted);
+    }
+
+    @Test
+    public void sort_nullDateDesc_nullLast() {
+        UserPost userPost1 = createUserPost(OffsetDateTime.of(20, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost2 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost3 = createUserPost(OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 1, ZoneOffset.UTC));
+        UserPost userPost4 = createUserPost(OffsetDateTime.of(2012, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost5 = createUserPost(OffsetDateTime.of(2020, 12, 31, 7, 12, 3, 0, ZoneOffset.UTC));
+        UserPost userPost6 = createUserPost(null);
+
+        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4, userPost5, userPost6);
+        List<UserPost> sortedBtMethod = sortByDate.sort(userPosts, SortOrder.DESC);
+        List<UserPost> sorted = Arrays.asList(userPost5, userPost3, userPost2, userPost4, userPost1, userPost6);
+        Assert.assertEquals(sortedBtMethod, sorted);
+    }
 }
