54f3a623ee36a2925f6717cbf0b2f0817bccf4f2
amohanan-scottlogic <amohanan@scottlogic.com>
2021-12-10 16:11:54 +0000

    Author Filter completed

--- src/test/java/com/scottlogic/AuthorFilterTest.java
+++ src/test/java/com/scottlogic/AuthorFilterTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class AuthorFilterTest {
 
-    AuthorFilter filterByAuthor = new AuthorFilter();
+
     private UserPost createUserPost(String name) {
 
         return new UserPost(name,
@@ -20,37 +20,51 @@ public class AuthorFilterTest {
 
     @Test
     public void filter_firstNameUppercase_correctListReturned() {
+        AuthorFilter filterByAuthor = new AuthorFilter("JOE");
         UserPost userPost1 = createUserPost("Joe Bloggs");
         UserPost userPost2 = createUserPost("Joe Bloggs");
         UserPost userPost3 = createUserPost("May Bloggs");
         UserPost userPost4 = createUserPost("Joe Blogg");
         List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4);
-        List<UserPost> actualResult = filterByAuthor.filter(userPosts, "JOE");
+        List<UserPost> actualResult = filterByAuthor.filter(userPosts);
         List<UserPost> expectedResult = Arrays.asList(userPost1, userPost2, userPost4);
         Assert.assertEquals(actualResult, expectedResult);
     }
 
     @Test
     public void filter_lastName_correctListReturned() {
+        AuthorFilter filterByAuthor = new AuthorFilter("Blogg");
         UserPost userPost1 = createUserPost("Joe Bloggs");
         UserPost userPost2 = createUserPost("Joe Bloggs");
         UserPost userPost3 = createUserPost("May Bligg");
         UserPost userPost4 = createUserPost("Joe Blogg");
         List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4);
-        List<UserPost> actualResult = filterByAuthor.filter(userPosts, "Blogg");
+        List<UserPost> actualResult = filterByAuthor.filter(userPosts);
         List<UserPost> expectedResult = Arrays.asList(userPost1, userPost2, userPost4);
         Assert.assertEquals(actualResult, expectedResult);
     }
 
     @Test
-    public void filter_nullrequested_emptyListReturned() {
+    public void filter_nullRequested_emptyListReturned() {
+        AuthorFilter filterByAuthor = new AuthorFilter(null);
         UserPost userPost1 = createUserPost("Joe Bloggs");
         UserPost userPost2 = createUserPost("Joe Bloggs");
-        UserPost userPost3 = createUserPost("May Bligg");
-        UserPost userPost4 = createUserPost("Joe Blogg");
-        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3, userPost4);
-        List<UserPost> actualResult = filterByAuthor.filter(userPosts, null);
+
+        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2);
+        List<UserPost> actualResult = filterByAuthor.filter(userPosts);
         Assert.assertTrue(actualResult.isEmpty());
+    }
+
+    @Test
+    public void filter_spaceRequested_correctListReturned() {
+        AuthorFilter filterByAuthor = new AuthorFilter(" ");
+        UserPost userPost1 = createUserPost("Joe Bloggs");
+        UserPost userPost2 = createUserPost("Joe Bloggs");
+
+        List<UserPost> userPosts = Arrays.asList(userPost1, userPost2);
+        List<UserPost> actualResult = filterByAuthor.filter(userPosts);
+        List<UserPost> expectedResult = Arrays.asList(userPost1, userPost2);
+        Assert.assertEquals(actualResult, expectedResult);
     }
 
 }
