ff7a1e95012d88b2e76081bb9b5be68feb7add75
amohanan-scottlogic <amohanan@scottlogic.com>
2022-01-13 08:09:46 +0000

    Associative property of filterAnd

--- src/test/java/com/scottlogic/filters/FilterANDTest.java
+++ src/test/java/com/scottlogic/filters/FilterANDTest.java
@@ -21,7 +21,7 @@ public class FilterANDTest {
 
     UserPost userPost2 = new UserPost("Joe Bloggs",
             OffsetDateTime.of(2020, 1, 3, 8, 53, 34, 0, ZoneOffset.UTC),
-            "Another example post.", 1);
+            "Another example post.", 0);
 
     UserPost userPost3 = new UserPost("Joane Smith",
             OffsetDateTime.of(2020, 3, 12, 13, 22, 12, 0, ZoneOffset.UTC),
@@ -58,7 +58,7 @@ public class FilterANDTest {
     }
 
     @Test
-public void filter_commutativeProperty_sameListDifferentOrderReturned() {
+    public void filter_commutativeProperty_sameListDifferentOrderReturned() {
 
         AuthorFilter filterByAuthor = new AuthorFilter("Jo");
         ContentFilter filterByContent = new ContentFilter("post");
@@ -69,12 +69,31 @@ public void filter_commutativeProperty_sameListDifferentOrderReturned() {
         List<UserPost> filter1Result = filteringAnd.filter(userPosts);
         List<UserPost> filter2Result = filteringAnd2.filter(userPosts);
 
-        Assert.assertTrue(CollectionUtils.isEqualCollection(filter1Result,filter2Result));
+        Assert.assertTrue(CollectionUtils.isEqualCollection(filter1Result, filter2Result));
     }
+
+    @Test
+    public void filter_associativeProperty_sameListDifferentOrderReturned() {
+
+        AuthorFilter filterByAuthor = new AuthorFilter("Jo");
+        ContentFilter filterByContent = new ContentFilter("post");
+        LikesFilter filterByLikes = new LikesFilter();
+        FilterAND filteringAnd = new FilterAND(filterByAuthor, filterByContent);
+        FilterAND filteringAnd2 = new FilterAND(filteringAnd, filterByLikes);
+        FilterAND filteringAndB = new FilterAND(filterByLikes, filterByAuthor);
+        FilterAND filteringAndB2 = new FilterAND(filteringAndB, filterByContent);
+
+
+        List<UserPost> actualResultA = filteringAnd2.filter(userPosts);
+        List<UserPost> actualResultB = filteringAndB2.filter(userPosts);
+
+        Assert.assertTrue(CollectionUtils.isEqualCollection(actualResultA, actualResultB));
+    }
+
     @Test
     public void filter_nullAndAuthor_emptyListReturned() {
         AuthorFilter filterByAuthor = new AuthorFilter("Jo");
-        ContentFilter filterByContent =new ContentFilter(null);
+        ContentFilter filterByContent = new ContentFilter(null);
         FilterAND filteringAnd = new FilterAND(filterByAuthor, filterByContent);
 
         List<UserPost> actualResult = filteringAnd.filter(userPosts);
